c8d7dfa8e3bc0bf1e1f581b2e3d5a2005faf9314
Li Yuan <liyuan@howadata.com>
2016-11-11 17:51:30 +0800

    add a test for cardpay_apply: rsa signature and rsa encryption

--- api/tests/test_home.py
+++ api/tests/test_home.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
 import urllib
 import operator
 import hashlib
+from base64 import b64encode
 
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_v1_5
+from Crypto.Hash.SHA import SHA1Hash
 import pytest
 
 import wsgi_handler
+import config
 
 
 class TestCardpayApply(object):
@@ -43,14 +45,48 @@ class TestCardpayApply(object):
         params_with_sign = params + [("sign", sign)]
         urlencoded_params = urllib.urlencode(params_with_sign)
 
-        pub_key = open(
-            os.path.join(
-                os.path.split(
-                    os.path.split(os.path.realpath(__file__))[0])[0],
-                "config", "id_isa_pub.pub")).read()
+        jidui_pub_key = RSA.importKey(config.JIDUI_PUB_KEY)
+        cipher = PKCS1_v1_5.new(jidui_pub_key)
+        cipher_data = pkcs_encrypt(cipher, urlencoded_params)
 
-        pub_key = RSA.importKey(pub_key)
-        cipher = PKCS1_v1_5.new(pub_key)
+        final_params = {"cipher_data": cipher_data}
+        final_params = urllib.urlencode(final_params)
+
+        resp = client.get('/cardpay/apply?%s' % final_params)
+
+        assert resp.status_code == 200
+
+    def test_cardpay_apply_rsa(self, client):
+        u"""RSA签名 + RSA加密"""
+
+        # 参数
+        params = {
+            "c": "",
+            "a": "xx",
+            "b": "xx",
+        }
+
+        params = params.items()
+        params = [(k, v) for k, v in params if
+                  v is not None and v != ""]
+        params = sorted(params, key=operator.itemgetter(0))
+        urlencoded_params = urllib.urlencode(params)
+
+        # RSA签名
+        sha1 = SHA1Hash()
+        sha1.update(urlencoded_params)
+        sha1_params = sha1.digest()
+        merchant_private_key = RSA.importKey(config.TEST_MERCHANT_PRIVATE_KEY)
+        cipher = PKCS1_v1_5.new(merchant_private_key)
+        cipher_data = pkcs_encrypt(cipher, sha1_params)
+        sign = b64encode(cipher_data)
+
+        # RSA加密
+        params_with_sign = params + [("sign", sign)]
+        urlencoded_params = urllib.urlencode(params_with_sign)
+
+        jidui_pub_key = RSA.importKey(config.JIDUI_PUB_KEY)
+        cipher = PKCS1_v1_5.new(jidui_pub_key)
         cipher_data = pkcs_encrypt(cipher, urlencoded_params)
 
         final_params = {"cipher_data": cipher_data}
