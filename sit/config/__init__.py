b91cb5b48fd91d5567a7feb2d382c485c2cdf230
Li Yuan <liyuan@howadata.com>
2017-02-21 18:28:41 +0800

    add sit(system integration test) project

---
+++ sit/config/__init__.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+"""
+SplitSettings for fenle project.
+
+__init__.py merges default.py, <FENLE_SETTINGS>.py in that order.
+
+default.py is a default config module.
+
+FENLE_SETTINGS is a env, it can be set in
+uwsgi config file:
+* local, for development enviroment.
+* test, for testing environment.
+* online, for online enviroment.
+
+"""
+
+import os
+from config_global import *
+
+
+def deep_update(from_dict, to_dict):
+    for (key, value) in from_dict.items():
+        if key in to_dict.keys() and \
+                isinstance(to_dict[key], dict) and \
+                isinstance(value, dict):
+            deep_update(value, to_dict[key])
+        else:
+            to_dict[key] = value
+
+
+modules = ["default"]
+env = os.environ.get("FENLE_SETTINGS")
+if env:
+    modules.append(env)
+
+current = __name__
+for module_name in modules:
+    try:
+        module = getattr(__import__(current, globals(), locals(),
+                                    [module_name]), module_name)
+    except AttributeError:
+        continue
+
+    module_fg = {}
+    for fg in dir(module):
+        if fg.startswith("__") and fg.endswith("__"):
+            continue
+        module_fg[fg] = getattr(module, fg)
+    deep_update(module_fg, locals())
---
+++ sit/views/home.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import datetime
+
+from flask import Blueprint
+from flask import request
+import gevent
+import requests
+
+from base.framework import general, db_conn, form_check
+from base.framework import JsonOkResponse, TempResponse
+from base.db import engine, t_callback_url
+from base import util
+from base import logic
+from base import logger
+from base.xform import F_str, F_int
+from base import constant as const
+
+home = Blueprint("home", __name__)
+
+
+@home.route("/")
+@general("首页")
+def index():
+    return TempResponse("index.html")
